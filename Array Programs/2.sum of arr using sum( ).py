fa9305ecd026152dcd1cc63f54ad7f9451498216
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-09-16 17:36:17 +0530

    Add files via upload

---
+++ Array Programs/2.sum of arr using sum( ).py
@@ -0,0 +1,9 @@
+arr = [12, 3, 4, 15]
+ 
+# sum() is an inbuilt function in python that adds
+# all the elements in list,set and tuples and returns
+# the value
+ans = sum(arr)
+ 
+# display sum
+print('Sum of the array is ', ans)

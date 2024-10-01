932c490beb54485143320de28616798fa70ed76a
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-10-01 23:28:45 +0530

    Add files via upload

---
+++ String methods/Methods/11.String isalnum().py
@@ -0,0 +1,11 @@
+string = "abc 123"
+print(string, "is alphanumeric?", string.isalnum())
+
+string = "abc_123"
+print(string, "is alphanumeric?", string.isalnum())
+
+string = "000"
+print(string, "is alphanumeric?", string.isalnum())
+
+string = "aaaa"
+print(string, "is alphanumeric?", string.isalnum())

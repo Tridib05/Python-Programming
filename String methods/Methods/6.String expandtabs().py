f47dffaf6e1d836ba8d8f69e74c8050238ec549b
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-10-01 23:24:12 +0530

    Update and rename 7.remove characters from string using reduce.py to 6.String expandtabs().py

---
+++ String methods/Methods/6.String expandtabs().py
@@ -0,0 +1,7 @@
+# initializing string
+string = "i\tlove\tgist"
+ 
+# using expandtabs to insert spacing
+print("Modified string using default spacing: ", end ="")
+print(string.expandtabs())
+print()

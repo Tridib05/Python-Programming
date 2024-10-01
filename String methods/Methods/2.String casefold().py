cd99ee4e99471e3d2dca05cbe7796d819ce15283
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-10-01 23:17:27 +0530

    Update and rename 2.check a string palindrome or not using inbulit function.py to 2.String casefold().py

---
+++ String methods/Methods/2.String casefold().py
@@ -0,0 +1,5 @@
+string = "WeightAGE oF THe DiScuSSiON"
+
+print("Original String:", string)
+# print the string after casefold transformation
+print("String after using casefold():", string.casefold())
